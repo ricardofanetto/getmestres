ac3b28e42e0cdd986ba497370ddf28e85ff45814
Ricardo Netto <ricardofanetto@gmail.com>
2019-02-21 17:48:56 -0400

    configuração do banco e refatoração do index.ts

---
+++ getMestres.Api/src/index.ts
@@ -0,0 +1,34 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { Request, Response } from "express";
+import { Routes } from "./routes";
+import config from "./configuration/config";
+
+// create express app
+const app = express();
+app.use(bodyParser.json());
+
+// register express routes from defined application routes
+Routes.forEach(route => {
+    (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+        const result = (new (route.controller as any))[route.action](req, res, next);
+        if (result instanceof Promise) {
+            result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
+
+        } else if (result !== null && result !== undefined) {
+            res.json(result);
+        }
+    });
+});
+
+app.listen(config.port, '0.0.0.0', async () => {
+    console.log(`Api initilze in port ${config.port}`);
+    try {
+        await createConnection();
+        console.log('Database connected');
+    } catch (error) {
+        console.error('Data base not connected', error);
+    }
+})
