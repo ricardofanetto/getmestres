c9dc8e4d899d5bf293bb2897706c295d452b5407
Ricardo Netto <ricardofanetto@gmail.com>
2019-02-26 21:49:14 -0400

    Aula 16 - Configurando as rotas públicas de o debug no VS Code

--- getMestres.Api/src/configuration/config.ts
+++ getMestres.Api/src/configuration/config.ts
@@ -1,4 +1,8 @@
 export default {
-  port: process.env.PORT || 3000, 
-  secretyKey: process.env.SECRETYKEY || 'f5ccc0f3-cbb6-4788-b4c8-403c22cd110f'
+  port: process.env.PORT || 3000,
+  secretyKey: process.env.SECRETYKEY || 'f5ccc0f3-cbb6-4788-b4c8-403c22cd110f',
+  publicRoutes: process.env.PUBLICROUTES || [
+    'users/create',
+    'users/auth'
+  ]
 }
--- getMestres.Api/src/middlaware/auth.ts
+++ getMestres.Api/src/middlaware/auth.ts
@@ -5,19 +5,30 @@ import config from "../configuration/config";
 export default async (req: Request, res: Response, next: Function) => {
 
   let token = req.body.token || req.query.token || req.headers['x-token-access'];
+  let publicRoutes = <Array<String>>config.publicRoutes;
+  let isPublicRoute: boolean = false;
 
-  if (token) {
-    try {
-      let _userAuth = verify(token, config.secretyKey);
-      req.userAuth = _userAuth;
-      next();
-    } catch (error) {
-      res.status(401).send({ message: 'Token informado é inválido' });
+  publicRoutes.forEach(url => {
+    let isPublic = req.url.includes(url);
+    if (isPublic)
+      isPublicRoute = true;
+  });
+
+  if (isPublicRoute)
+    next();
+  else
+    if (token) {
+      try {
+        let _userAuth = verify(token, config.secretyKey);
+        req.userAuth = _userAuth;
+        next();
+      } catch (error) {
+        res.status(401).send({ message: 'Token informado é inválido' });
+        return;
+      }
+    } else {
+      res.status(401).send({ message: 'Para acessar esse recurso você precisa estar autenticado' });
       return;
     }
-  } else {
-    res.status(401).send({ message: 'Para acessar esse recurso você precisa estar autenticado' });
-    return;
-  }
 
 }
