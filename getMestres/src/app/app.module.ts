93e562bbb47417323de507ebb4e24f01c246a6fc
Ricardo Netto <ricardofanetto@gmail.com>
2019-05-15 19:18:45 -0400

    Aula 26 - Criação do Projeto Administrativo: :

---
+++ getMestres/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MatToolbarModule, MatIconModule,
+  MatButtonModule, MatSidenavModule,
+  MatListModule
+} from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
